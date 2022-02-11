11218eed24e8ce45fba0792fdae3f67b9e235ba1
Gabriel Argente <gaarag@floridauniversitaria.es>
2022-02-11 15:17:08 +0100

    Update Actividad 2

---
+++ ACTIVIDADES COMPLEMENTARIA/ACTCOMPLEMENTARIA2/Pasajero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTCOMPLEMENTARIA2
+{
+    class Pasajero
+    {
+        private string nif;
+        private string nombre;
+        private string apellido;
+        private string nacionalidad;
+        private string fechaNacimiento;
+        private int numMaletas;
+
+        public Pasajero(string nif, string nom, string ape, string nac, string fech, string num)
+        {
+            this.nif = nif;
+            nombre = nom;
+            apellido = ape;
+            nacionalidad = nac;
+            fechaNacimiento = fech;
+            numMaletas = num;
+        }
+
+    }
+}
---
+++ ACTIVIDADES COMPLEMENTARIA/ACTCOMPLEMENTARIA2/Vuelo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTCOMPLEMENTARIA2
+{
+    class Vuelo
+    {
+        private int codVuelo;
+        private string fechaVuelo;
+        private string horaVuelo;
+        private string ciudadOri;
+        private string ciudadDes;
+        private int capMaxPasajeros;
+        private List<Pasajero> pasajeros;
+
+        public Vuelo(int cod, string fech, string ho, string cidO, string cidD, int cap, List<Pasajero> pas)
+        {
+            codVuelo = cod;
+            fechaVuelo = fech;
+            horaVuelo = ho;
+            ciudadOri = cidO;
+            ciudadDes = cidD;
+            capMaxPasajeros = cap;
+            pasajeros = pas;
+        }
+    }
+}
